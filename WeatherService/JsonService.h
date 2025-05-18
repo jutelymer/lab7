756cb4e676e304367c5cd51e3fcef85c31169422
Victoria Kokovina <jutelymer00@mail.ru>
2025-05-18 15:59:13 +0300

    fix

---
+++ WeatherService/JsonService.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <fstream>
+#include "Service.h"
+#include "json.hpp"
+
+using nlohmann::json;
+
+class JsonService : public Service {
+public:
+	virtual Weather getWeather(std::string s) override;
+	virtual ~JsonService() {};
+};
